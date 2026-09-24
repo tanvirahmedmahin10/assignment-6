f7fc98bdaf6c0b9e12f889e807a1df8ca1631949
tanvirahmedmahin10 <tanvirahmedmahinsarker@gmail.com>
2026-09-24 22:23:51 +0600

    Not found added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,20 @@
-import { Link } from 'lucide-react';
+
+import Link from 'next/link';
 import React from 'react';
 
 const NotFound = () => {
     return (
        <div className="flex min-h-[60vh] flex-col items-center justify-center text-center">
-  <h2 className="text-2xl font-bold">
-    Nothing Here yet
+  <h2 className="text-9xl font-bold">
+    404
   </h2>
 
-  <p className="mt-2 text-gray-400">
-    Browse the library and add a lift to get today moving.
+  <p className="my-6 text-4xl text-gray-400">
+    Not Found
   </p>
   <Link href='/'>
   <button className="mt-5 rounded-lg bg-[#C2F10D] px-5 py-2 font-semibold text-black cursor-pointer">
-    Go to fsfdf
+    Go back to HomePage
   </button>
   </Link>
 </div>
